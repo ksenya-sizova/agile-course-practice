c1c242e16453675e269d6b45d84b322b02d8b1bc
Anna-Mironova <anyt.ka@bk.ru>
2019-12-15 17:55:24 +0300

    Added Infrastructure layer, TxtLogger and TxtLoggerTests classes

---
+++ code/temperature-converter/Infrastructure/src/main/java/ru/unn/agile/temperatureconverter/infrastructure/TxtLogger.java
@@ -0,0 +1,66 @@
+package ru.unn.agile.temperatureconverter.infrastructure;
+
+import ru.unn.agile.temperatureconverter.viewmodel.ILogger;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+public class TxtLogger implements ILogger {
+    private String filename;
+    private final BufferedWriter writer;
+    private static final String DATE_FORMAT_NOW = "yyyy-MM-dd HH:mm:ss";
+
+    public TxtLogger(final String filename) {
+        this.filename = filename;
+
+        BufferedWriter logWriter = null;
+        try {
+            logWriter = new BufferedWriter(new FileWriter(filename));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        writer = logWriter;
+    }
+
+    private static String now() {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW, Locale.ENGLISH);
+        return sdf.format(cal.getTime());
+    }
+
+    @Override
+    public void log(final String message) {
+        try {
+            writer.write(now() + " > " + message);
+            writer.newLine();
+            writer.flush();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public List<String> getLog() {
+        BufferedReader reader;
+        ArrayList<String> log = new ArrayList<String>();
+        try {
+            reader = new BufferedReader(new FileReader(filename));
+            String line = reader.readLine();
+
+            while (line != null) {
+                log.add(line);
+                line = reader.readLine();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return log;
+    }
+}
