a1ba012350e80b42705b4007e6e07dd545ae244e
Ksenya Sizova <sizova.ksuniy@mail.ru>
2019-12-22 19:12:58 +0300

    Added one more tests for txtlogger

--- code/queue/Infrastructure/src/test/java/ru/unn/agile/queue/infrastructure/TxtLoggerTests.java
+++ code/queue/Infrastructure/src/test/java/ru/unn/agile/queue/infrastructure/TxtLoggerTests.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -43,4 +44,31 @@ public class TxtLoggerTests {
         String logMessage = logger.getLog().get(0);
         assertTrue(logMessage.matches(".*" + message + "$"));
     }
+
+    @Test
+    public void canLogSeveralMessages() {
+        String[] messages = {"info", "warning", "error"};
+
+        logger.log(messages[0]);
+        logger.log(messages[1]);
+        logger.log(messages[2]);
+
+        List<String> logMessages = logger.getLog();
+        for (int i = 0; i < logMessages.size(); i++) {
+            String actualMessage = logMessages.get(i);
+            assertTrue(actualMessage.matches(".*" + messages[i] + "$"));
+        }
+    }
+
+    @Test
+    public void isDateAndTimeAddedToMessageCorrectly() {
+        String message = "test";
+
+        logger.log(message);
+
+        String logMessage = logger.getLog().get(0);
+        var format = "^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2} > .*";
+        assertTrue(message.matches(format));
+    }
+
 }
